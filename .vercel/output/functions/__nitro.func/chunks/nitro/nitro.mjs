16c3c512cfcbd09ad6d2667d9c5d178415f9baa9
chetan dhargalkar <chetan.dmc@gmail.com>
2025-01-30 12:08:32 +0530

    gitupdated

--- .vercel/output/functions/__nitro.func/chunks/nitro/nitro.mjs
+++ .vercel/output/functions/__nitro.func/chunks/nitro/nitro.mjs
@@ -4602,7 +4602,7 @@ function _expandFromEnv(value) {
 const _inlineRuntimeConfig = {
   "app": {
     "baseURL": "/",
-    "buildId": "6510c164-ba14-4e07-a018-d76ac78ab49e",
+    "buildId": "241b8147-9b88-45ca-942a-4a90c1e65f00",
     "buildAssetsDir": "/_nuxt/",
     "cdnURL": ""
   },
@@ -5421,11 +5421,11 @@ async function dispose(driver) {
 const _assets = {
   ["nuxt-og-image:fonts:Inter-400.ttf.base64"]: {
     import: () => import('../raw/Inter-400.ttf.mjs').then(r => r.default || r),
-    meta: {"type":"text/plain; charset=utf-8","etag":"\"652cc-qEeSD1DXCSV8gPP2rnBA6ePGdZ4\"","mtime":"2025-01-30T06:33:17.109Z"}
+    meta: {"type":"text/plain; charset=utf-8","etag":"\"652cc-qEeSD1DXCSV8gPP2rnBA6ePGdZ4\"","mtime":"2025-01-30T06:38:15.062Z"}
   },
   ["nuxt-og-image:fonts:Inter-700.ttf.base64"]: {
     import: () => import('../raw/Inter-700.ttf.mjs').then(r => r.default || r),
-    meta: {"type":"text/plain; charset=utf-8","etag":"\"674f0-FZReUXHhPTnY0HmYVn2iPpKm9ds\"","mtime":"2025-01-30T06:33:17.109Z"}
+    meta: {"type":"text/plain; charset=utf-8","etag":"\"674f0-FZReUXHhPTnY0HmYVn2iPpKm9ds\"","mtime":"2025-01-30T06:38:15.062Z"}
   }
 };
 
